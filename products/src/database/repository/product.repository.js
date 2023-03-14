1f288656fde4e7c496919b2291953b6c5bb166b3
DESKTOP-3VG7I1R\Vinh Ngo <vinhtrungngo1907@gmail.com>
2023-03-14 07:54:17 +0700

    commit

--- products/src/database/repository/product.repository.js
+++ products/src/database/repository/product.repository.js
@@ -1,3 +1,23 @@
+const { ProductModel } = require("../models");
+
 class ProductRepository {
-    
+    async Products() {
+        return await ProductModel.find();
+    }
+
+    async FindById(id) {
+        return await ProductModel.findById(id);
+    }
+
+    async FindByCategory(category) {
+        const prodcts = await ProductModel.find({ type: category });
+        return prodcts;
+    }
+
+    async FindSelectedProducts(selectedIds) {
+        const products = await ProductModel.find().where('_id').in(selectedIds.map(_id => _id)).exec();
+        return products;
+    }
 }
+
+module.exports = ProductRepository;
