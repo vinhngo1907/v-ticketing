2758d8d95f3ffc9307d54e678917219f1ff05b4a
DESKTOP-3VG7I1R\Vinh Ngo <vinhtrungngo1907@gmail.com>
2023-03-18 11:30:13 +0700

    [fix] - some bugs

--- customer/src/api/customer.js
+++ customer/src/api/customer.js
@@ -1,20 +1,20 @@
 const CustomerService = require("../services/customer-service");
-const UserAuth = require("./middleware/auth");
+const { UserAuth, Admin } = require("./middleware");
 const { SubscribeMessage } = require('../utils');
 const { BASE_URL } = require("../configs");
 
 module.exports = (app, channel = "") => {
     const service = new CustomerService();
 
-    app.post(BASE_URL+"/customer/login", async (req, res) => {
-        const { email, password, phone } = req.body;
-        const { data } = await service.Login({ email, password, phone });
+    app.post(BASE_URL + "/customer/login", async (req, res) => {
+        const { email, password } = req.body;
+        const data = await service.Login({ email, password });
         res.json(data)
     });
 
-    app.post(BASE_URL+"/customer/register", async (req, res) => {
+    app.post(BASE_URL + "/customer/register", async (req, res) => {
         const { email, password, phone, fullname } = req.body;
-        const { data } = await service.Register({ email,fullname, password, phone });
+        const { data } = await service.Register({ email, fullname, password, phone });
         res.json(data);
     })
 }
