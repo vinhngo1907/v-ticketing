1d2a2a5023ac875b39f85aa34325946a3dada4d7
DESKTOP-3VG7I1R\Vinh Ngo <vinhtrungngo1907@gmail.com>
2023-03-18 16:44:01 +0700

    commit

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,10 @@
+// which service it is
+describe("CustomerService", () => {
+    // Which function
+    describe("Login", () => {
+        // Which Scenario we are testing
+        test("validate user inputs", () => { });
+
+        test("Validate response", async () => { });
+    });
+});
