8fa8fa7538338068cff6c7eecc92eb8acef06d2d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-03-26 22:13:50 +0700

    re config source backend

--- backend/products/src/app.module.ts
+++ backend/products/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './product/client/client.module';
+import { ProductModule } from './product/product.module';
+import { ClientModule } from './client/client.module';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ClientModule],
+  imports: [ClientModule, ProductModule, SharedModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
