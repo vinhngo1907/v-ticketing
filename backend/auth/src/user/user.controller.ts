0ae3d117da4d62137b39a59b25723541f2002d35
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-03-27 00:11:05 +0700

    commit

--- backend/auth/src/user/user.controller.ts
+++ backend/auth/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, UseGuards, Get } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService,
+        // @Inject('CLIENT_SERVICE') private readonly client: ClientProxy
+        // @Inject('PRODUCT_SERVICE') private readonly client1: ClientProxy
+    ) { }
+    @Get()
+    // @UseGuards(new AuthGuard())
+    showAllUsers() {}
+}
