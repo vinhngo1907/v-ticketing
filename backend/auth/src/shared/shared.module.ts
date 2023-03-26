5fdcd20b8d6fae1397fdb5958ee2f7c52617912e
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-03-26 22:27:56 +0700

    commit

--- backend/auth/src/shared/shared.module.ts
+++ backend/auth/src/shared/shared.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { SharedController } from './shared.controller';
 import { SharedService } from './shared.service';
 
 @Module({
-  controllers: [UserController, SharedController],
-  providers: [UserService, SharedService]
+  controllers: [SharedController],
+  providers: [ SharedService]
 })
 export class SharedModule {}
