7abacb25e922c45e1dec005c1676dd1dc088494c
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-03-30 16:30:46 +0700

    commit

--- backend/auth/src/app.module.ts
+++ backend/auth/src/app.module.ts
@@ -5,9 +5,10 @@ import { IdeaModule } from './idea/idea.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { SharedModule } from './shared/shared.module';
+import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
-  imports: [IdeaModule, UserModule, DatabaseModule, SharedModule],
+  imports: [IdeaModule, UserModule, DatabaseModule, SharedModule, KafkaModule],
   controllers: [AppController],
   providers: [AppService],
 })
