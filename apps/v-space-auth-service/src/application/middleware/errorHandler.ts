1aa400c405da5f60267d0ffdf32f0a1cf58e2f0e
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-27 17:43:29 +0700

    [readme]

--- apps/v-space-auth-service/src/application/middleware/errorHandler.ts
+++ apps/v-space-auth-service/src/application/middleware/errorHandler.ts
@@ -1,11 +1,7 @@
-const { Signature } = require("../../utils");
-const signature = new Signature();
-
-module.exports = async (req, res, next) => {
-    const isAuthorized = await signature.ValidateSignature(req);
-
-    if (isAuthorized) {
-        return next();
-    }
-    return res.status(403).json({ message: "Not Authorized" });
-}
+import {
+    NextFunction,
+    Request,
+    Response,
+    ValidateError,
+  } from '@v-libs/node-infrastructure';
+  
