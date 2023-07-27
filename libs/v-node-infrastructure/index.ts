b5d036e4b1692a018a2b3ae7dc8a7c01eb56fffd
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-27 17:36:10 +0700

    init v-node-omfrastrictire

--- libs/v-node-infrastructure/index.ts
+++ libs/v-node-infrastructure/index.ts
@@ -1,15 +1,2 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/', (req,res,next) => {
-
-    return res.status(200).json({"msg": "Hello from Shopping"})
-})
-
-
-app.listen(8003, () => {
-    console.log('Shopping is Listening to Port 8003')
-});
+/// <reference path="./global.d.ts" />
+export * from './src';
